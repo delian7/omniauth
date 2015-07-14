244e135df4e9fd55c092d3df7e597386ea327d7e
Delian Petrov <petrovd@uci.edu>
2015-07-14 08:25:59 -0700

    move google authentication to secrets.yml

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -240,7 +240,7 @@ Devise.setup do |config|
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
   require 'omniauth-google-oauth2'
-  config.omniauth :google_oauth2, "596426331624-iujllsul8lfjdrss3hlf1slrdaglpeo2.apps.googleusercontent.com", "N9dmVikaY53DMuVVIht4OYmO", { access_type: "offline", approval_prompt: "" }
+  config.omniauth :google_oauth2, Rails.application.secrets.google_application_id, Rails.application.secrets.google_secret, { access_type: "offline", approval_prompt: "" }
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
